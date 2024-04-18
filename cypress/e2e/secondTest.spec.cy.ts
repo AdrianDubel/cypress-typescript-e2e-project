b79c178f9077e2899cbc26af31620ffe6dbc1c20
AdrianDubel <adrian.dubel88@gmail.com>
2024-04-18 11:39:29 +0200

    add second failed test

---
+++ cypress/e2e/secondTest.spec.cy.ts
@@ -0,0 +1,16 @@
+import loginPage from "../support/pages/login.page";
+import homePage from "../support/pages/home.page";
+
+describe("Login tests", () => {
+  it("Login using correct credentials", () => {
+    const username = Cypress.env("USERNAME");
+    const password = Cypress.env("PASSWORD");
+
+    cy.visit("/");
+    loginPage.login(username, password);
+    homePage.elements
+      .userName()
+      .should("be.visible")
+      .and("contain.text", 'dupa');
+  });
+});
